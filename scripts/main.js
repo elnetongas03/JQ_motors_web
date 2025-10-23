d11f49115a061eab8aef8e1f6db6511a045f931e
ERNESTO ISAAC <pachados.03@gmail.com>
2025-10-23 12:34:11 -0600

    Actualización del index y slider automático

--- scripts/main.js
+++ scripts/main.js
@@ -105,3 +105,18 @@ saveBtn.addEventListener("click", () => {
 // Cargar JSON al presionar el botón
 loadBtn.addEventListener("click", cargarCatalogo);
 
+// === SLIDER AUTOMÁTICO ===
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Cambia cada 4 segundos
+}
+
